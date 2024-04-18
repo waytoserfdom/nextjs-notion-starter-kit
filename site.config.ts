433bdb09813efc41c896fed897f4e386537d5852
waytoserfdom <140362789+waytoserfdom@users.noreply.github.com>
2024-04-18 14:13:49 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,11 +2,11 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '62f612ccdc014524b0d46e19ff8382a1',
+  rootNotionPageId: 'b25a33301ad04450b655d7dd874e5aef',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: 'b25a33301ad04450b655d7dd874e5aef',
+  rootNotionSpaceId: null,
 
   // basic site info (required)
   name: 'IT Service Wiki',
